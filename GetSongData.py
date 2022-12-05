c11b56dde0662a69db0e592571f86f52f486cdbd
Kate McCurley <cmccurley@olin.edu>
2022-12-05 09:55:52 -0500

    reformat data reading

--- GetSongData.py
+++ GetSongData.py
@@ -16,7 +16,7 @@ sp = spotipy.Spotify(client_credentials_manager = client_cred_manager)
 playlist_url = "https://open.spotify.com/playlist/64QxysD2w5x5EMLgcoT7fa"
 uri = playlist_url.split("/")[-1].split("?")[0]
 
-song_data = {
+'''song_data = {
     "name": [],
     "artist": [],
     "URI": [],
@@ -24,19 +24,22 @@ song_data = {
     "genre": [],
     "energy": [],
     "key": []
-}
+}'''
+
+song_data = []
 
 i = 0
 
 for o in range(5):
     for track in sp.playlist_tracks(uri, limit = 100, offset = o * 100)["items"]:
+        song = {}
         # URI
         track_uri = track["track"]["uri"]
-        song_data["URI"].append(track_uri)
+        song["URI"] = (track_uri)
 
         # track name
         track_name = track["track"]["name"]
-        song_data["name"].append(track_name)
+        song["name"] = (track_name)
 
         # artist info
         artist_uri = track["track"]["artists"][0]["uri"]
@@ -45,14 +48,16 @@ for o in range(5):
         artist_name = track["track"]["artists"][0]["name"]
         artist_genres = artist_info["genres"]
 
-        song_data["artist"].append(artist_name)
-        song_data["genre"].append(artist_genres)
+        song["artist"] = (artist_name)
+        song["genre"] = (artist_genres)
 
         # audio features
         features = sp.audio_features(track_uri)[0]
-        song_data["tempo"].append(features["tempo"])
-        song_data["energy"].append(features["energy"])
-        song_data["key"].append(features["key"])
+        song["tempo"] = (features["tempo"])
+        song["energy"] = (features["energy"])
+        song["key"] = (features["key"])
+
+        song_data.append(song)
 
         print(i)
         i += 1
