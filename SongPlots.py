74c81d3482391a334393fe649fb536cef240b943
Kate McCurley <cmccurley@olin.edu>
2022-12-11 23:15:38 -0500

    generate plots of song characteristics

---
+++ SongPlots.py
@@ -0,0 +1,27 @@
+import json
+import matplotlib.pyplot as plt
+
+# read song data
+with open("data.json") as f:
+    data = json.load(f)
+
+
+# generate tempo plot
+tempo_vals = [song["tempo"] for song in data]
+
+plt.hist(tempo_vals, edgecolor = "grey", color = "pink")
+plt.title("Tempo Values for Top 500 Songs of 2022", fontsize = 20)
+plt.xlabel("Tempo (bpm)", fontsize = 14)
+plt.ylabel("Number of Songs", fontsize = 14)
+plt.show()
+
+
+
+# generate energy level plot
+energy_vals = [song["energy"] for song in data]
+
+plt.hist(energy_vals, edgecolor = "grey", color = "pink")
+plt.title("Energy Values for Top 500 Songs of 2022", fontsize = 20)
+plt.xlabel("Energy Level (0 to 1)", fontsize = 14)
+plt.ylabel("Number of Songs", fontsize = 14)
+plt.show()
